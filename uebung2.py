c7773471d1606741729b62b7df4cceaa5f660e4c
Roechi <msg.remus@gmail.com>
2015-04-28 18:48:54 +0200

    Uebung 2 angefange, Aufgabe 2 a.) geloest (in uebung2.py)

---
+++ uebung2.py
@@ -0,0 +1,33 @@
+#Loesung fuer 2 a.)
+
+import numpy as np
+
+import agents
+
+runs = np.array(range(100))
+
+print "\n+++TrivialVacuumEnvironment+++\n"
+
+reflex_vacuum_agent = agents.ReflexVacuumAgent()
+
+for key, value in enumerate(runs):
+    vacuum_environment = agents.TrivialVacuumEnvironment()
+    vacuum_environment.add_thing(reflex_vacuum_agent)
+    vacuum_environment.run(10)
+    runs[key] = reflex_vacuum_agent.performance
+
+print "\nReflexVacuumAgent:\n"
+print runs
+print("\n\nAverage: " + str(runs.sum() / len(runs)))
+
+model_vacuum_agent = agents.ModelBasedVacuumAgent()
+
+for key, value in enumerate(runs):
+    vacuum_environment = agents.TrivialVacuumEnvironment()
+    vacuum_environment.add_thing(model_vacuum_agent)
+    vacuum_environment.run(10)
+    runs[key] = model_vacuum_agent.performance
+
+print "\nModelBasedVacuumAgent:\n"
+print(runs)
+print("\n\nAverage: " + str(runs.mean()))
