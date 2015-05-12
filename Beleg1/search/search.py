a3d98fa36cb85880ff78e445632e81dbca91e683
Roechi <msg.remus@gmail.com>
2015-05-12 16:21:11 +0200

    Refactored uniformCostSearch

--- Beleg1/search/search.py
+++ Beleg1/search/search.py
@@ -103,35 +103,8 @@ def uniformCostSearch(problem):
     "Search the node of least total cost first. "
     "*** YOUR CODE HERE ***"
     #util.raiseNotDefined()
-    import Node    
-
-    fringe = util.PriorityQueue()
-    solution = []
-    closed = []
-    root = Node.Node()
-    root.setParent(None)
-    root.setAction(None)
-    root.setState(problem.getStartState())
-    root.setPathCost(0)
-    root.setDepth(0)
-
-    fringe.push(root, root.pathcost)
-    while (not fringe.isEmpty()):
-        node = fringe.pop()
-        if problem.isGoalState(node.state):
-            current = node
-            solution = []
-            while (not(current.parent is None)):
-                solution.append(current.action)
-                current = current.parent
-            solution.reverse()
-            return solution
-        else:
-            if (not closed.__contains__(node.state)):
-                closed.append(node.state)
-                for n in Node.expand(node, problem):
-                    fringe.push(n, n.pathcost)
-    return []
+    import uniformCostSearch
+    return uniformCostSearch.uniformCostSearch(problem, util.PriorityQueue())
 
 def nullHeuristic(state, problem=None):
     """
---
+++ Beleg1/search/uniformCostSearch.py
@@ -0,0 +1,29 @@
+import Node    
+
+def uniformCostSearch(problem, fringe):
+    solution = []
+    closed = []
+    root = Node.Node()
+    root.setParent(None)
+    root.setAction(None)
+    root.setState(problem.getStartState())
+    root.setPathCost(0)
+    root.setDepth(0)
+
+    fringe.push(root, root.pathcost)
+    while (not fringe.isEmpty()):
+        node = fringe.pop()
+        if problem.isGoalState(node.state):
+            current = node
+            solution = []
+            while (not(current.parent is None)):
+                solution.append(current.action)
+                current = current.parent
+            solution.reverse()
+            return solution
+        else:
+            if (not closed.__contains__(node.state)):
+                closed.append(node.state)
+                for n in Node.expand(node, problem):
+                    fringe.push(n, n.pathcost)
+    return []
